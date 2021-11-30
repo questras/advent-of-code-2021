d0e7dcaf51418eded8f9ca7b5d7678cdfd22a8b4
Michal Wisniewski <michu123qwe@gmail.com>
2021-11-30 19:30:48 +0100

    Add new lines to add day script.

--- add_day.sh
+++ add_day.sh
@@ -6,6 +6,10 @@ mkdir $dirpath
 touch $filepath
 
 echo "package ${day}\n" >> $filepath
+echo "import java.io.File\n" >> $filepath
 echo "fun main(args: Array<String>) {" >> $filepath
+echo "    val lines = File(\"files/${day}.txt\").readLines()" >> $filepath
 echo "    println(\"Hello World on ${day}!\")" >> $filepath
 echo "}" >> $filepath
+
+touch files/${day}.txt
--- src/main/kotlin/day1/day1.kt
+++
@@ -1,5 +0,0 @@
-package day1
-
-fun main(args: Array<String>) {
-    println("Hello World on day1!")
-}
