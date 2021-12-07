e7168e5145fe9a440c1deb44660f2cc8fb36a76e
Michal Wisniewski <michu123qwe@gmail.com>
2021-12-07 09:31:23 +0100

    Refactor.

--- src/main/kotlin/day6/day6.kt
+++ src/main/kotlin/day6/day6.kt
@@ -2,58 +2,14 @@ package day6
 
 import java.io.File
 
-// f(n) = n/6
-
-fun g(n: Int): Int {
-    var sum = 1
-    var newN = n
-    if (n < 8) {
-        return sum
-    }
-
-    sum += g(n-8)
-    newN -= 8
-
-    while (newN > 5) {
-        sum += g(newN)
-        newN -= 6
-    }
-
-    return sum
-}
-
-fun f(n: Int, first: Int): Int {
-    var sum = 1
-    var newN = n
-    if (n < first) {
-        return sum
-    }
-
-    sum += g(n-first)
-    newN -= first
-
-    while (newN > 5) {
-        sum += g(newN)
-        newN -= 6
-    }
-
-    return sum
-}
-
-fun main(args: Array<String>) {
-    val lines = File("files/day6.txt").readLines()[0].split(",").map { it.toInt() }
-
-    val states = lines.toMutableList()
-    val numberOfDays = 256
-
+fun solve(input: List<Int>, numberOfDays: Int): Long {
     var freqVec = mutableListOf<Long>(0, 0, 0, 0, 0, 0, 0, 0, 0)
-    for (s in states) {
+    for (s in input) {
         freqVec[s]++
     }
-    println(freqVec)
 
     for (i in 0 until numberOfDays) {
-        var newVec = mutableListOf<Long>(0, 0, 0, 0, 0, 0, 0, 0, 0)
+        val newVec = mutableListOf<Long>(0, 0, 0, 0, 0, 0, 0, 0, 0)
 
         newVec[8] = freqVec[0]
         newVec[7] = freqVec[8]
@@ -68,9 +24,13 @@ fun main(args: Array<String>) {
         freqVec = newVec
     }
 
-    println(freqVec.sum())
+    return freqVec.sum()
+}
 
+fun main(args: Array<String>) {
+    val lines = File("files/day6.txt").readLines()[0].split(",").map { it.toInt() }
 
-
+    println(solve(lines, 80))
+    println(solve(lines, 256))
     println("Hello World on day6!")
 }
